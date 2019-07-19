496c03c315f977bfe5184afd5fa0dae6fabe4f00
莫吉托 <1340645315@qq.com>
2019-07-19 19:31:50 +0800

    在hello.java中添加了test方法

--- hello.java
+++ hello.java
@@ -2,6 +2,8 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("欢迎来到莫吉托的网站");
     }
-
-	
+	//测试方
+	public void test(){
+		System.out.println("这是个测试方法");
+	}
 }
